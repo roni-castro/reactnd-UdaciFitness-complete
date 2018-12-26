256461124cd6c075e9661c3198a050fffe5cf52f
Roni <roni.c.castro@gmail.com>
2018-12-26 14:41:40 -0200

    Add status bar specific to each platform

--- App.js
+++ App.js
@@ -4,6 +4,8 @@ import Tabs from './components/Tabs'
 import { View } from 'react-native';
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
+import UdaciStatusBar from './components/UdaciStatusBar';
+import { purple } from './utils/colors';
 
 export default class App extends React.Component {  
 
@@ -11,6 +13,7 @@ export default class App extends React.Component {
     return (
       <Provider store={createStore(reducers)}>
         <View style={{flex: 1}}>
+          <UdaciStatusBar backgroundColor={purple} barStyle='light-content' />
           <Tabs/>
         </View>
       </Provider>
